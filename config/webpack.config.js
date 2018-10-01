15a8af52b722dfbdcede7f2cc788c089c788cd05
hieki <ksakahieki@gmail.com>
2018-10-01 23:50:05 +0900

    [WIP] init
    TS と Webpack と Electron の環境は適当に用意したけど `npm start` すると electron がうまくトランスパイル後のコードを読んでくれないあたりまで作業した

---
+++ config/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  entry: "./ts/index.ts",
+  cache: true,
+  devtool: "source-map",
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: "ts-loader"
+    }, {
+      test: /\.tsx?$/,
+      enforce: "pre",
+      loader: "tslint-loader",
+      options: {
+        configFile: "./tslint.json",
+        typeCheck: true,
+      },
+    }],
+  },
+  resolve: {
+    extensions: [
+      ".ts",
+      ".tsx",
+      ".js",
+    ]
+  },
+};
---
+++ ts/react/test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const container = document.getElementById("contents");
+
+ReactDom.render(
+    <p>hello, oracle!</p>,
+    container,
+);
